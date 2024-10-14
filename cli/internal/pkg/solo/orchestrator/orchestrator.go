b43a85591d10326a2f45ff59fe3fcc7c1bc4aaad
Simon Paulger <spaulger@codezen.co.uk>
2024-10-14 21:58:56 +0100

    orchestrator abstraction

--- cli/internal/pkg/solo/orchestrator/orchestrator.go
+++ cli/internal/pkg/solo/orchestrator/orchestrator.go
@@ -1,11 +1,16 @@
 package orchestrator
 
+import (
+	"github.com/spaulg/solo/cli/internal/pkg/solo/config"
+)
+
 type Orchestrator interface {
 	Start(projectDirectory string, composeFile string) error
 	Stop(projectDirectory string, composeFile string) error
 	Destroy(projectDirectory string, composeFile string) error
+	ExportComposeConfiguration(globalConfig *config.Config, projectPath string) ([]byte, error)
 }
 
 func BuildOrchestrator() Orchestrator {
-	return &DockerComposeOrchestrator{}
+	return &DockerOrchestrator{}
 }
