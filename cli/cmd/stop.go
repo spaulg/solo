307c477ec782487177a1416df1f7bd78cf463a17
Simon Paulger <spaulger@codezen.co.uk>
2024-10-14 18:14:16 +0100

    Refactor variable names

--- cli/cmd/stop.go
+++ cli/cmd/stop.go
@@ -16,8 +16,8 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		project := solo.NewProjectControl(globalConfig, projectFile)
-		project.Stop()
+		projectControl := solo.NewProjectControl(config, project)
+		projectControl.Stop()
 	},
 }
 
