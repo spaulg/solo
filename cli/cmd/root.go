d33537627ad6c3be02393fd8adc64440d0a1b1e4
Simon Paulger <spaulger@codezen.co.uk>
2024-10-15 13:15:14 +0100

    return errors from project control

--- cli/cmd/root.go
+++ cli/cmd/root.go
@@ -15,17 +15,8 @@ var projectLoadErr, configLoadErr error
 
 // rootCmd represents the base command when called without any solo
 var rootCmd = &cobra.Command{
-	Use: "solo",
-	//Short: "A brief description of your application",
-	//Long: `A longer description that spans multiple lines and likely contains
-	//examples and usage of using your application. For example:
-	//
-	//Cobra is a CLI library for Go that empowers applications.
-	//This application is a tool to generate the needed files
-	//to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Use:          "solo",
+	SilenceUsage: true,
 	PersistentPreRun: func(cmd *cobra.Command, args []string) {
 		if projectLoadErr != nil {
 			fmt.Println(projectLoadErr)
--- cli/internal/pkg/solo/project_control.go
+++ cli/internal/pkg/solo/project_control.go
@@ -26,15 +26,22 @@ func NewProjectControl(config *config.Config, projectFile *project.Project) *Pro
 	}
 }
 
-func (p *ProjectControl) DumpComposeConfig() {
-	composeYml, _ := p.Orchestrator.ExportComposeConfiguration(p.Config, p.Project.FilePath)
+func (p *ProjectControl) DumpComposeConfig() error {
+	composeYml, err := p.Orchestrator.ExportComposeConfiguration(p.Config, p.Project.FilePath)
+	if err != nil {
+		return err
+	}
+
 	fmt.Println(string(composeYml))
+	return nil
 }
 
-func (p *ProjectControl) Start() {
+func (p *ProjectControl) Start() error {
 	// Write compose file
 	composeYml, _ := p.Orchestrator.ExportComposeConfiguration(p.Config, p.Project.FilePath)
-	p.exportComposeFile(composeYml)
+	if err := p.exportComposeFile(composeYml); err != nil {
+		return err
+	}
 
 	// todo: launch provisioning grpc server
 	//fmt.Println("Launching GRPC service...")
@@ -42,8 +49,7 @@ func (p *ProjectControl) Start() {
 	//go grpc_server.Listen()
 
 	if err := p.Orchestrator.Up(p.Project.Directory, p.ComposeFile); err != nil {
-		fmt.Println(fmt.Errorf("error running composeCmd: %v", err))
-		os.Exit(1)
+		return fmt.Errorf("error running composeCmd: %v", err)
 	}
 
 	//fmt.Println("Sleeping...")
@@ -53,61 +59,69 @@ func (p *ProjectControl) Start() {
 	// todo: wait delay period for final containers to start
 	// todo: Exec post start commands (via docker exec)
 	// todo: wait delay period for all containers to checkin for post start commands provisioning
+
+	return nil
 }
 
-func (p *ProjectControl) Stop() {
-	p.composeFileExists()
+func (p *ProjectControl) Stop() error {
+	if err := p.composeFileExists(); err != nil {
+		return err
+	}
 
 	// todo: Exec pre stop commands
 
 	if err := p.Orchestrator.Down(p.Project.Directory, p.ComposeFile); err != nil {
-		fmt.Println(fmt.Errorf("error running compose: %v", err))
+		return fmt.Errorf("error running compose: %v", err)
 	}
+
+	return nil
 }
 
-func (p *ProjectControl) Destroy() {
-	p.composeFileExists()
+func (p *ProjectControl) Destroy() error {
+	if err := p.composeFileExists(); err != nil {
+		return err
+	}
 
 	// todo: Exec pre stop commands
 
 	if err := p.Orchestrator.Destroy(p.Project.Directory, p.ComposeFile); err != nil {
-		fmt.Println(fmt.Errorf("error running compose: %v", err))
+		return fmt.Errorf("error running compose: %v", err)
 	}
 
 	if err := os.Remove(p.ComposeFile); err != nil {
-		fmt.Println(fmt.Errorf("failed to remove compose file: %v", err))
-		os.Exit(1)
+		return fmt.Errorf("failed to remove compose file: %v", err)
 	}
+
+	return nil
 }
 
-func (p *ProjectControl) exportComposeFile(composeYml []byte) {
+func (p *ProjectControl) exportComposeFile(composeYml []byte) error {
 	composeDirectory := path.Dir(p.ComposeFile)
 	if _, err := os.Stat(composeDirectory); err != nil {
 		if !errors.Is(err, os.ErrNotExist) {
-			fmt.Println(fmt.Errorf("failed to check .solo directory existence: %v", err))
-			os.Exit(1)
+			return fmt.Errorf("failed to check .solo directory existence: %v", err)
 		}
 
 		if err := os.MkdirAll(composeDirectory, 0755); err != nil {
-			fmt.Println(fmt.Errorf("failed to create .solo directory: %v", err))
-			os.Exit(1)
+			return fmt.Errorf("failed to create .solo directory: %v", err)
 		}
 	}
 
 	if err := os.WriteFile(p.ComposeFile, composeYml, 0640); err != nil {
-		fmt.Println(fmt.Errorf("failed to write compose file: %v", err))
-		os.Exit(1)
+		return fmt.Errorf("failed to write compose file: %v", err)
 	}
+
+	return nil
 }
 
-func (p *ProjectControl) composeFileExists() {
+func (p *ProjectControl) composeFileExists() error {
 	if _, err := os.Stat(p.ComposeFile); err != nil {
 		if errors.Is(err, os.ErrNotExist) {
-			fmt.Println("compose file not found")
-			os.Exit(1)
+			return fmt.Errorf("compose file not found")
 		} else {
-			fmt.Println(fmt.Errorf("error running composeCmd: %v", err))
-			os.Exit(1)
+			return fmt.Errorf("error looking for compose file: %v", err)
 		}
 	}
+
+	return nil
 }
