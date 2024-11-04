5370fb8f675b33d4533f72ff3f26e93a7448ac0d
Simon Paulger <spaulger@codezen.co.uk>
2024-11-04 12:56:04 +0000

    test

--- src/cli/internal/pkg/solo/grpc/certificate_generator_test.go
+++ src/cli/internal/pkg/solo/grpc/certificate_generator_test.go
@@ -1,10 +1,28 @@
 package grpc
 
+import (
+	"errors"
+	"testing"
+)
+
 // error validation
 //   empty hostname
 //   invalid hostname
 //   empty certificate base path
 //   invalid certificate base path
+
+func TestEmptyHostname(t *testing.T) {
+	_, err := NewCertificateGenerator("", "")
+	if err == nil {
+		// fail
+	}
+
+	if !errors.Is(err, InvalidHostname) {
+		// fail
+	}
+
+	// pass
+}
 
 // happy path tests
 //   the certificate files are generated in the base directory
