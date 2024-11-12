c1e93bae2a7b7cd8a9a4b2c3db67cb077695d410
Simon Paulger <spaulger@codezen.co.uk>
2024-11-12 19:37:34 +0000

    remoev tests

--- src/solo/internal/pkg/solo/grpc/server_test.go
+++
@@ -1,9 +0,0 @@
-package grpc
-
-// error validation
-//   with blank hostname
-//   with port of 0
-//   with blank state directory
-
-// happy path tests
-//   the server starts and can be connected to
