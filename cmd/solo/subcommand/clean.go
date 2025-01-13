6cdfca7e18ed77624f545f7c7521189ebd6109d1
Simon Paulger <spaulger@codezen.co.uk>
2025-01-13 18:25:23 +0000

    Implement lock files on certain cli commands to prevent parameter execs
    Implement lock files on certain cli commands to prevent parameter execs

--- cmd/solo/subcommand/clean.go
+++ cmd/solo/subcommand/clean.go
@@ -1,9 +1,11 @@
 package subcommand
 
 import (
+	"errors"
 	"github.com/spaulg/solo/internal/pkg/solo"
 	"github.com/spaulg/solo/internal/pkg/solo/context"
 	"github.com/spf13/cobra"
+	"os"
 )
 
 func NewCleanSubCommand(soloCtx *context.SoloContext) *cobra.Command {
@@ -11,6 +13,9 @@ func NewCleanSubCommand(soloCtx *context.SoloContext) *cobra.Command {
 		Use:   "clean",
 		Short: "Clean the app",
 		Long:  "Clean the app",
+		PreRunE: func(cmd *cobra.Command, args []string) error {
+			return soloCtx.TryLock()
+		},
 		RunE: func(cmd *cobra.Command, args []string) error {
 			projectControl, err := solo.ProjectControlFactory(soloCtx)
 			if err != nil {
@@ -18,6 +23,15 @@ func NewCleanSubCommand(soloCtx *context.SoloContext) *cobra.Command {
 			}
 
 			return projectControl.Clean(true)
+		},
+		PostRunE: func(cmd *cobra.Command, args []string) error {
+			if err := soloCtx.Unlock(); err != nil {
+				if !errors.Is(err, os.ErrNotExist) {
+					return err
+				}
+			}
+
+			return nil
 		},
 	}
 }
