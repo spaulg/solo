264ab69ce47e3f2de778c95a3c232035e1b9d033
Simon Paulger <spaulger@codezen.co.uk>
2025-01-24 22:34:53 +0000

    Refactor and fix config test file
    Refactor and fix config test file

--- internal/pkg/solo/config/config_test.go
+++ internal/pkg/solo/config/config_test.go
@@ -2,59 +2,39 @@ package config
 
 import (
 	_ "github.com/spaulg/solo/test"
+	asserter "github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestConfigLoading(t *testing.T) {
+	assert := asserter.New(t)
 	config, err := NewConfig()
-	if err != nil {
-		t.Fatalf("Failed to load config without error: %v", err)
-	}
 
-	if config.Entrypoint != DefaultEntrypoint {
-		t.Fatal("Entrypoint does not match default")
-	}
-
-	if config.StateDirectoryName != DefaultStateDirectoryName {
-		t.Fatal("StateDirectoryName does not match default")
-	}
+	assert.Nil(err, "Failed to load config without error: %v", err)
+	assert.Equal(DefaultEntrypoint, config.Entrypoint, "Entrypoint does not match default")
+	assert.Equal(DefaultStateDirectoryName, config.StateDirectoryName, "StateDirectoryName does not match default")
 
 	if err := config.AddConfigPath("test/data/config"); err != nil {
-		t.Fatalf("failed to add config path: %v", err)
+		assert.Fail("failed to add config path: %v", err)
 	}
 
-	if config.Entrypoint != "/opt/bin/solo-custom-entrypoint.sh" {
-		t.Fatalf("Entrypoint %s does not match overridden config %s", config.Entrypoint, "/opt/bin/solo-custom-entrypoint.sh")
-	}
-
-	//if config.StateDirectoryName != "/opt/solo" {
-	//	t.Fatalf("StateDirectoryName %s does not match overridden config %s", config.StateDirectoryName, "/opt/solo")
-	//}
+	assert.Equal("/opt/bin/solo-custom-entrypoint.sh", config.Entrypoint, "Entrypoint %s does not match overridden config")
+	assert.Equal("/opt/solo", config.StateDirectoryName, "StateDirectoryName %s does not match overridden config")
 }
 
 func TestConfigPathNotFound(t *testing.T) {
+	assert := asserter.New(t)
 	config, err := NewConfig()
-	if err != nil {
-		t.Fatalf("Failed to load config without error: %v", err)
-	}
 
-	if config.Entrypoint != DefaultEntrypoint {
-		t.Fatal("Entrypoint does not match default")
-	}
+	assert.Nil(err, "Failed to load config without error: %v", err)
 
-	if config.StateDirectoryName != DefaultStateDirectoryName {
-		t.Fatal("StateDirectoryName does not match default")
-	}
+	assert.Equal(DefaultEntrypoint, config.Entrypoint, "Entrypoint does not match default")
+	assert.Equal(DefaultStateDirectoryName, config.StateDirectoryName, "StateDirectoryName does not match default")
 
 	if err := config.AddConfigPath("test/data/config/notfound"); err != nil {
-		t.Fatalf("failed to add config path: %v", err)
+		assert.Fail("failed to add config path: %v", err)
 	}
 
-	if config.Entrypoint != DefaultEntrypoint {
-		t.Fatal("Entrypoint does not match default")
-	}
-
-	if config.StateDirectoryName != DefaultStateDirectoryName {
-		t.Fatal("StateDirectoryName does not match default")
-	}
+	assert.Equal(DefaultEntrypoint, config.Entrypoint, "Entrypoint does not match default")
+	assert.Equal(DefaultStateDirectoryName, config.StateDirectoryName, "StateDirectoryName does not match default")
 }
