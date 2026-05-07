da6665b8323b56e6ec63f9c014e8dee06b5e09d0
Simon Paulger <simon@paulger.uk>
2026-05-07 20:21:54 +0100

    Move WorkflowGuardFactory interface to the point of consumption
    Move WorkflowGuardFactory interface to the point of consumption

--- internal/pkg/types/host/app/wms/workflow_guard_factory.go
+++
@@ -1,9 +0,0 @@
-package wms
-
-import (
-	workflowcommon "github.com/spaulg/solo/internal/pkg/impl/common/domain/wms"
-)
-
-type WorkflowGuardFactory interface {
-	Build(workflowNames []workflowcommon.WorkflowName, containerNames []string) WorkflowGuard
-}
