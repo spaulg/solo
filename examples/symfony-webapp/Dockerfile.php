bcacc3234467a4de7516aa82a2c9bbe2246acfda
Simon Paulger <simon@paulger.uk>
2026-06-06 20:28:31 +0100

    Upgrade Symfony example to 8.1 and PHP 8.5
    Upgrade Symfony example to 8.1 and PHP 8.5

--- examples/symfony-webapp/Dockerfile.php
+++ examples/symfony-webapp/Dockerfile.php
@@ -1,4 +1,4 @@
-FROM spaulg/php-dev-containers:8.4
+FROM spaulg/php-dev-containers:8.5
 
 RUN echo "deb http://deb.debian.org/debian bookworm-backports main" >> /etc/apt/sources.list.d/bookworm-backports.list
 RUN apt update -y
